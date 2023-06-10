ae87f8f2333628afdb580bb1cf16cafcb1d02bcb
khoican <ibnukhoirulprasetyo@gmail.com>
2023-06-10 08:01:51 +0700

    menambahkan warna pada tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'lapizBlue': "#33658A",
+        'carolinaBlue': "#86BBD8",
+        'mossGreen': "#758E4F",
+        'hunyadiYellow': "#F6AE2D",
+        'pentatonOrange': "#F26419"
+      }
+    },
   },
   plugins: [],
 }
