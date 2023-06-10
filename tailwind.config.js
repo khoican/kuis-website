d97f6ec6394c67429ae8bbf89776473b2d9df3ac
khoican <ibnukhoirulprasetyo@gmail.com>
2023-06-10 13:27:34 +0700

    menamahkan google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         'mossGreen': "#758E4F",
         'hunyadiYellow': "#F6AE2D",
         'pentatonOrange': "#F26419"
+      },
+      fontFamily: {
+        'Poppins': ["Poppins"]
       }
     },
   },
